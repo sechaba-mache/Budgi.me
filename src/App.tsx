25675147d3686bdb73f6290a03afb41e8bdfdd7e
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-03-17 15:07:29 +0200

    Init Swiper and update home page with top section and drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css';
 import './App.css'
+import { Pagination } from 'swiper/modules'
+import 'swiper/css/pagination';
 import Card from './components/card/card'
 
 function App() {
 
   return (
     <>
-      <Card></Card>
+      <Swiper
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true, type: 'bullets' }}
+        >
+          <SwiperSlide><Card></Card><span>February</span></SwiperSlide>
+          <SwiperSlide><Card></Card><span>February</span></SwiperSlide>
+          <SwiperSlide><Card></Card><span>February</span></SwiperSlide>
+      </Swiper>
+
+      <div className='drawer'>
+        <div className='gripper'></div>
+      </div>
     </>
   )
 }
