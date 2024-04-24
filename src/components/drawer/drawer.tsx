11da0b9d461a6c66af883298f3058777c7dc34f3
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-04-24 11:36:51 +0200

    Update drawer with tailwind styling

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -30,9 +30,9 @@ export default function Drawer(props: { children: string | number | boolean | Re
 
     return (
     <>
-        <div className='drawer' ref={drawerElement}>
-            <div className='gripper' onClick={() => openDrawer()}>
-                <div className='bar'></div>
+        <div className='drawer flex flex-col w-full h-[95%] rounded-t-[22px] bg-white absolute z-50 overflow-y-scroll' ref={drawerElement}>
+            <div className='flex items-center justify-center min-h-8 w-full h-8 sticky top-0 bg-white' onClick={() => openDrawer()}>
+                <div className='bar w-14 h-2 rounded-lg bg-[#023047]'></div>
             </div>
             <div className='px-4 pb-5'>
                 {props.children}
